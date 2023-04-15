6dfe2bac09674233136568a42ac7d83f5336ab4d
Carlos Mougan <carmougan@gmail.com>
2023-04-15 11:36:19 +0200

    Validation Set

--- syntheticBenchmark.py
+++ syntheticBenchmark.py
@@ -3,7 +3,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 plt.style.use("seaborn-whitegrid")
 from matplotlib import rcParams
@@ -16,28 +15,17 @@ rcParams.update({"font.size": 22})
 
 # Sklearn
 from sklearn.model_selection import train_test_split
-from sklearn.datasets import make_blobs
 from sklearn.linear_model import Lasso
-from sklearn.metrics import (
-    roc_auc_score,
-    accuracy_score,
-    mean_absolute_error,
-    mean_squared_error,
-)
-from sklearn.datasets import make_regression
-
-
-# Custom libraries
-from tools.PlugIn import PlugInRule
+from sklearn.metrics import mean_absolute_error
 
 # Doubt
 from doubt import Boot
 
 # %%
 # Create data
-n = 1000
-X = np.random.normal(0, 1, n)
-y = X + 10 * np.sin(1.2 * X) + np.random.normal(0, 1, n)
+n = 2000
+X = np.random.normal(0, 2, n) * 10
+y = X + 10 * np.sin(0.5 * X) + np.random.normal(0, 1, n)
 # Convert to dataframe
 # df = pd.DataFrame(X, columns=["Var%d" % (i + 1) for i in range(X.shape[1])])
 df = pd.DataFrame([X]).T
@@ -87,55 +75,11 @@ X_val_["y"] = y_val
 X_val_["y_hat"] = clf.predict(X_val)
 
 # %%
-mae = []
-mse = []
-cov_list = np.linspace(0.001, 0.4, 10)
-for coverage in cov_list:
-    X_te_["selected"] = np.where(
-        X_te_["interval"] > np.quantile(interval_val, 1 - coverage), 1, 0
-    )
-    aux = X_te_[X_te_["selected"] == 0].copy()
-    print(
-        "Coverage {:.2f} samples selected {}, which is {:.2f}%".format(
-            coverage,
-            X_te_[X_te_["selected"] == 0].shape[0],
-            X_te_[X_te_["selected"] == 0].shape[0] / X_te_.shape[0],
-        )
-    )
-    mae.append(mean_absolute_error(aux.y, aux.y_hat))
-    mse.append(mean_squared_error(aux.y, aux.y_hat))
-
-    # Plot X, Y, model line and the uncertainty interval
-    if True == True:
-        plt.figure(figsize=(10, 10))
-        plt.scatter(X_te.Var1, y_te, alpha=0.1)
-        plt.plot(X_te.Var1, clf.predict(X_te), color="red")
-        plt.scatter(
-            X_te_[X_te_["selected"] == 1].Var1,
-            X_te_[X_te_["selected"] == 1].y,
-            alpha=0.1,
-            color="green",
-        )
-        plt.show()
-
-
-# %%
-# Plot MAE and MSE
-plt.figure(figsize=(10, 10))
-plt.plot(cov_list, mae, label="MAE")
-plt.plot(cov_list, mse, label="MSE")
-plt.ylabel("Error (Lower is better)")
-plt.xlabel("Coverage")
-plt.legend()
-plt.show()
-
-# %%
 # Benchmark against others
-
 unc = []
 var = []
 err = []
-cov_list = np.linspace(0.001, 0.99, 10)
+cov_list = np.linspace(0.1, 0.9, 10)
 for coverage in cov_list:
     print("---------------")
     # Uncertainty with Doubt
@@ -143,14 +87,16 @@ for coverage in cov_list:
     X_te_["uncertainty"] = np.where(
         X_te_["interval"] > np.quantile(interval_val, 1 - coverage), 1, 0
     )
+    unc.append(mean_absolute_error(aux.y, aux.y_hat))
 
     aux = X_te_[X_te_["uncertainty"] == 0].copy()
     print(
-        "Doubt: Coverage {:.2f} samples selected {}".format(
-            coverage, X_te_[X_te_["uncertainty"] == 0].shape[0]
+        "Doubt: Coverage {:.2f} samples selected {} with error {:.2f}".format(
+            coverage,
+            X_te_[X_te_["uncertainty"] == 0].shape[0],
+            mean_absolute_error(aux.y, aux.y_hat),
         )
     )
-    unc.append(mean_absolute_error(aux.y, aux.y_hat))
 
     # Gold Case - Best possible
     error = (y_te - clf.predict(X_te)) ** 2
@@ -159,8 +105,10 @@ for coverage in cov_list:
     err.append(mean_absolute_error(aux0.y, aux0.y_hat))
 
     print(
-        "Gold: Coverage {:.2f} samples selected {}".format(
-            coverage, X_te_[X_te_["error"] == 0].shape[0]
+        "Gold: Coverage {:.2f} samples selected {} with error {:.2f}".format(
+            coverage,
+            X_te_[X_te_["error"] == 0].shape[0],
+            mean_absolute_error(aux0.y, aux0.y_hat),
         )
     )
     # Variance
@@ -170,12 +118,14 @@ for coverage in cov_list:
     aux1 = X_te_[X_te_["variance"] == 0].copy()
     var.append(mean_absolute_error(aux1.y, aux1.y_hat))
     print(
-        "Variance: Coverage {:.2f} samples selected {}".format(
-            coverage, X_te_[X_te_["variance"] == 0].shape[0]
+        "Variance: Coverage {:.2f} samples selected {} with error {:.2f}".format(
+            coverage,
+            X_te_[X_te_["variance"] == 0].shape[0],
+            mean_absolute_error(aux1.y, aux1.y_hat),
         )
     )
     # Plot X, Y, model line and the uncertainty interval
-    if True == True:
+    if True == False:
         plt.figure(figsize=(10, 10))
         plt.scatter(X_te.Var1, y_te, alpha=0.1)
         plt.plot(X_te.Var1, clf.predict(X_te), color="red")
